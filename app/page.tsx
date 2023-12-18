a1c1a2a4ce2049bb7194dcb25dae0fc59693fd46
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2023-12-18 13:30:02 +0530

    Create Home Page
    - Create Dashboard
    - Add night theme
    - Create orders and lead sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,206 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { LogOut } from "lucide-react"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { ModeToggle } from "@/components/ui/toggle"
 
-export default function Home() {
+const Home = () => {
+
+  const DASHBOARD_DATA = [
+    {
+      id: 1,
+      title: "Total Sales",
+      description: "Sales of December",
+      data: 1243
+    },
+    {
+      id: 2,
+      title: "Total Sales",
+      description: "Sales of December",
+      data: 1243
+    },
+    {
+      id: 3,
+      title: "New Stock",
+      description: "Stock of December",
+      data: 1243
+    },
+    {
+      id: 4,
+      title: "Total Leads",
+      description: "Leads of December",
+      data: 1243
+    },
+    {
+      id: 5,
+      title: "Total Revenue",
+      description: "Revenue of this year",
+      data: 3_21_352
+    },
+    {
+      id: 6,
+      title: "Total Customer Served",
+      description: "Custome serverd till now",
+      data: 3200
+    }
+  ]
+  const LEAD_DATA = {
+    title: "Total Leads",
+    description: "Summary of lead recevied",
+    leadData: [
+      {
+        id: 1,
+        clientName: "Rajesh Kapoor",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reiciendis?",
+        quantity: 120
+      },
+      {
+        id: 2,
+        clientName: "Megha Kapoor",
+        message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reiciendis?",
+        quantity: 120
+      }
+    ]
+  }
+
+  const ORDER_DATA = {
+    title: "Total Orders",
+    description: "Summary of orders",
+    orderData: [
+      {
+        id: 1,
+        productName: "IRS Sensor",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reiciendis?",
+        quantity: 12
+      },
+      {
+        id: 2,
+        productName: "IRS Sensor",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, reiciendis?",
+        quantity: 12
+      }
+    ]
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className="border-b-2 flex bg-card justify-around py-3 items-center">
+        <h3>Made2Automate</h3>
+        <div className="flex gap-3 items-center">
+          <ModeToggle />
+          <Button size={"icon"} variant={"default"}>
+            <LogOut />
+          </Button>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="mx-auto max-w-7xl my-6 px-4">
+        <h1>Dashboard</h1>
+        <section className="items-start mt-6 justify-center xl:justify-start flex flex-wrap gap-6">
+          {
+            DASHBOARD_DATA.map((data) => {
+              return (
+                <DashBoardCard title={data.title} data={data.data} description={data.description} key={data.id} />
+              )
+            })
+          }
+        </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="mt-12">
+          <h1>Oders and Leads</h1>
+          <div className="mt-6 items-center gap-6 lg:flex-row flex-col flex justify-around">
+            <OrderCard title={ORDER_DATA.title} description={ORDER_DATA.description} data={ORDER_DATA.orderData} />
+            <LeadCard
+              title={LEAD_DATA.title}
+              description={LEAD_DATA.description}
+              data={LEAD_DATA.leadData}
+            />
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
+
+const DashBoardCard = ({ title, data, description }: { title: String, data: number, description: String }) => {
+  return (
+    <Card className="w-full sm:w-auto px-6 gap-6 flex items-center justify-between">
+      <CardHeader>
+        <CardTitle>
+          <h2>{title}</h2>
+        </CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="p-0">
+        <h3>{data}</h3>
+      </CardContent>
+    </Card>
+  )
+}
+
+const LeadCard = ({ title, description, data }: { title: String, description: String, data: Array<any> }) => {
+  return (
+    <Card className="max-w-md px-4 pt-2">
+      <section>
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <div className="flex-1">
+              <h2>{title}</h2>
+              <span>{description}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          {
+            data.map((item) => {
+              return (
+                <div key={item.id} className=" px-4 py-4 rounded hover:bg-accent">
+                  <h3>{item.clientName}</h3>
+                  <span>{item.message}</span>
+                  <p><b>Quantity:</b>{item.quantity}</p>
+                  <hr className="mt-4" />
+                </div>
+              )
+            })
+          }
+        </CardContent>
+        <CardFooter>
+          <Button>View all</Button>
+        </CardFooter>
+      </section>
+
+    </Card>
+  )
+}
+const OrderCard = ({ title, description, data }: { title: String, description: String, data: Array<any> }) => {
+  return (
+    <Card className="max-w-md px-4 pt-2">
+      <section>
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <div className="flex-1">
+              <h2>{title}</h2>
+              <span>{description}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          {
+            data.map((item) => {
+              return (
+                <div key={item.id} className=" px-4 py-4 rounded hover:bg-accent">
+                  <h3>{item.productName}</h3>
+                  <span>{item.description}</span>
+                  <p><b>Quantity:</b>{item.quantity}</p>
+                  <hr className="mt-4" />
+                </div>
+              )
+            })
+          }
+        </CardContent>
+      </section>
+      <CardFooter>
+        <Button>View all</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+export default Home
